9911d72e8afc90ee1aadf9870a0a8a103b848f43
Dominik Honnef <dominikh@fork-bomb.org>
2011-05-10 03:11:58 +0200

    use media keys on desktop pc

--- config/subtle/subtle.rb
+++ config/subtle/subtle.rb
@@ -11,7 +11,7 @@ BIG_PANEL       = HOST != "dominikh-netbook"
 CUSTOM_BRIGHTNESS_CONTROLS = HOST == "dominikh-laptop"
 VOLUME_CONTROL = case HOST
                  when "dominikh-lapop" then :fancy
-                 when "dominikh-netbook" then :simple
+                 when "dominikh-netbook", "dominikh-pc" then :simple
                  else :none
                  end
 
