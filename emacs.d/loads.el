51a84a3233ef465c5b84a42af8145be76fda063b
Dominik Honnef <dominik@honnef.co>
2016-04-10 15:25:35 +0200

    emacs: don't load modes that we don't use

--- emacs.d/loads.el
+++ emacs.d/loads.el
@@ -1,24 +1,8 @@
 (require 'compile)
-(require 'etags-select)
-
 (require 'tramp) ;; cannot autoload this
-(require 'unit-test)
-
-(require 'glasses)
-(require 'linum)
 (require 'which-func)
-
-(require 'iedit)
 (require 'recentf)
-
-(require 'android-mode)
-
-(require 'highlight-parentheses)
-
 (require 'elscreen)
-(require 'deft)
 (require 'rich-minority)
-
 (require 'key-chord)
-
 (require 'keyfreq)
