d600dacba36d65cef19605962fda988419fa87e5
Dominik Honnef <dominik@honnef.co>
2016-04-10 15:32:36 +0200

    emacs: remove unnecessary and broken autoloads

--- emacs.d/autoloads.el
+++ emacs.d/autoloads.el
@@ -1,42 +1,15 @@
-;; Lorem ipsum
-(autoload 'Lorem-ipsum-insert-list "lorem-ipsum" nil t)
-(autoload 'Lorem-ipsum-insert-paragraphs "lorem-ipsum" nil t)
-(autoload 'Lorem-ipsum-insert-sentences "lorem-ipsum" nil t)
+(autoload 'browse-url-interactive-arg "browse-url") ;; TODO why?
 
-(autoload 'browse-url-interactive-arg "browse-url")
-
-;; Major modes
-(autoload 'apt-sources-mode "apt-sources" nil t)
-(autoload 'csv-mode "csv-mode" nil t) ;; in elpa, but no autoload cookies
-(autoload 'espresso-mode "espresso" nil t)
-(autoload 'git-blame-mode "git-blame" nil t)
 (autoload 'ledger-mode "ledger-mode" nil t) ;; in site-lisp, but xbps doesn't generate autoloads files
+
+;; In contrib, thus not automatically autoloaded. Also, some files
+;; don't have autoload cookies at all.
 (autoload 'no-word "no-word" nil nil)
 (autoload 'procmail-mode "procmail" nil t) ;; contrib
-
-(autoload 'http-get "http-get" nil t)
-(autoload 'http-get-debug "http-get" nil t)
-
-
-
-(autoload 'kill-ring-search "kill-ring-search" nil t) ;; XXX broken/missing
-
-(autoload 'macro-math-eval-and-round-region "macro-math" nil t)
-(autoload 'macro-math-eval-region "macro-math" nil t)
-
 (autoload 'spelln-currency-in-words "spell-number")
 (autoload 'spelln-currency-string-in-words "spell-number")
 (autoload 'spelln-integer-in-words "spell-number")
 (autoload 'spelln-number-customize "spell-number")
 (autoload 'spelln-numeric-string-in-words "spell-number")
-
 (autoload 'textstats "textstats" "Display text statistics" t)
-
-(autoload 'mo-git-blame-file "mo-git-blame" nil t)
-(autoload 'mo-git-blame-current "mo-git-blame" nil t)
-
 (autoload 'vorbiscomment "vorbiscomment" nil t)
-
-;; (autoload 'magit-status "magit" nil t)
-
-(autoload 'jde-mode "jde" "JDE mode." t)
