922d181ec32eb2ce02c0e4800dc2a0bf7f9a8e9c
Dominik Honnef <dominik@honnef.co>
2016-04-10 13:42:09 +0200

    emacs: don't add .emacs.d to load path

--- emacs.d/loadpaths.el
+++ emacs.d/loadpaths.el
@@ -1,4 +1,3 @@
-(add-to-list 'load-path "~/.emacs.d")
 (add-to-list 'load-path "~/.emacs.d/contrib")
 (add-to-list 'load-path "~/.emacs.d/contrib/html5-el/")
 (add-to-list 'load-path "/usr/local/share/emacs/site-lisp")
