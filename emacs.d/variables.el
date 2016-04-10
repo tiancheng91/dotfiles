abf9072798390859d7daf35ef5d65832d51cf5e5
Dominik Honnef <dominik@honnef.co>
2016-04-10 13:07:07 +0200

    emacs: use chromium as browser

--- emacs.d/variables.el
+++ emacs.d/variables.el
@@ -23,7 +23,7 @@
 (put 'narrow-to-region 'disabled nil)
 (put 'downcase-region 'disabled nil)
 (put 'upcase-region 'disabled nil)
-(setq browse-url-generic-program "firefox")
+(setq browse-url-generic-program "chromium")
 (setq browse-url-browser-function 'browse-url-generic)
 (setq display-time-day-and-date 1)
 
